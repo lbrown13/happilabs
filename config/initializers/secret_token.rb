a74c60b79bb8b78443c591680a4312c1312ff36b
Lawrence Brown <lawrence.brown@gmail.com>
2013-05-11 10:06:55 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happilabs::Application.config.secret_token = '41a163e6fe1db48b848699ef56168bbd0ad96d750a458eb88877929d90e553be8ad29fc06b10a2e4a55ca3020f93e3515860ae61387b523403d0aa7e67b601b4'
